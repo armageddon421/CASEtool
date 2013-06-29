a30a2cad1af072bf8760750c30e5dffe36205c46
Patrick <patrick.thierfelder@gmail.com>
2013-06-29 19:05:49 +0200

    erster einfacher Unittest

--- ProjectCaseTool/test/FunctionPointMethodTest.java
+++ ProjectCaseTool/test/FunctionPointMethodTest.java
@@ -18,16 +18,25 @@ public class FunctionPointMethodTest {
 		FunctionPointMethod tester = new FunctionPointMethod();
 		Project project = new Project("neu");
 		project.addFunctionRequirement("f1");
-		Field expected = new Field("FP result", Type.String, false, this, "FP Ergebnis");
-		float weightedFP = 0;
-		float duration = 0;
-		float manpower = 0;
-		float effort = 0;
-		expected.addChild(new Field("Weighted FP", Type.Float, false, this, weightedFP));
-		expected.addChild(new Field("Duration", Type.Float, false, this, duration));
-		expected.addChild(new Field("Manpower", Type.Float, false, this, manpower));
-		expected.addChild(new Field("Effort", Type.Float, false, this, effort));
-		assertEquals("Dumm gelaufen", expected, tester.calculate(project));
+		
+		
+		Field result = tester.calculate(project);
+		float fp = (float) result.getChildren().get(0).getValue();
+		double fpresult = (double) fp;
+		
+		float duration = (float) result.getChildren().get(1).getValue(); 
+		double durResult = (double) duration;
+		
+		float manpower = (float) result.getChildren().get(2).getValue(); 
+		double manResult = (double) manpower;
+		
+		float effort = (float)  result.getChildren().get(3).getValue(); 
+		double effortResult = (double) effort;
+		
+		assertEquals("Gewichtete FP", 2.1, fpresult, 0.01);
+		assertEquals("Dauer", 1.3455, durResult, 0.1);
+		assertEquals("Personen", 0.014, manResult, 0.1);
+		assertEquals("Aufwand", 0.018837, effortResult, 0.1);
 	}
 
 }
